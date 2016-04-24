1ccac613337350427a56af4f9f6ad7e80a50e5cf
ZW <huang.zhongwen@gmail.com>
2016-04-25 02:36:47 +0800

    Added files via upload

---
+++ plot1.R
@@ -0,0 +1,12 @@
+df <- read.table("household_power_consumption.txt", skip = 66637, nrow = 2880, sep = ";")
+name <- sapply(read.table("household_power_consumption.txt", nrow = 1, sep = ";"), as.character)
+names(df) <- name
+df$DateTime <- strptime(paste(df$Date, df$Time), format="%d/%m/%Y %H:%M:%S")
+for( i in 3:9 ){
+        df[[i]] <- sapply(df[[i]], as.character)
+        df[[i]] <- sapply(df[[i]], as.numeric)
+}
+
+png(filename='plot1.png',width=480,height=480,units='px')
+hist(df$Global_active_power, xlab = "Global Active Power (kilowatts)", main = "Global Active Power", col = "red", ylim = c(0, 1200))
+x<-dev.off()
